d688456edea68d10fc0eb54edad39c5d717c74e6
Paul Delhom <paul.delhom@student-cs.fr>
2021-06-09 15:00:11 +0200

    c tro

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,8 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const moviesRouter = require("./routes/movies");
 const fillRouter = require("./routes/fill");
-<<<<<<< HEAD
 const filmRouter = require("./routes/film");
-=======
 const registerRouter = require("./routes/register")
->>>>>>> master
 const routeNotFoundJsonHandler = require("./services/routeNotFoundJsonHandler");
 
 mongoose.connect(process.env.MONGO_DB_URL, {
@@ -32,12 +29,9 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/movies", moviesRouter);
 app.use("/fill", fillRouter);
-<<<<<<< HEAD
 app.use("/film", filmRouter);
-=======
 app.use("/register", registerRouter)
 
->>>>>>> master
 app.use(routeNotFoundJsonHandler);
 
 const port = parseInt(process.env.PORT || "3000");
