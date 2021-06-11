6a35767125231653fa80ff244f04b2b321f20307
Paul Delhom <paul.delhom@student-cs.fr>
2021-06-11 13:14:32 +0200

    v1 recomm front

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,5 @@
 <template>
-  <div class="recom">
-    <div id="container-movies">
-      <ul>
-        <li v-for="movie in movies" :key="movie._id">
-          <router-link :to="`/film/${movie._id}`">
-            <Movie :movie="movie" />
-          </router-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <p>Vos recommandations :</p>
   <div class="milieu">
     <form id="search-form">
       <input
@@ -27,6 +17,16 @@
       </button>
     </form>
   </div>
+  <div id="container-movies">
+    <ul>
+      <li v-for="movie in moviesR" :key="movie._id">
+        <router-link :to="`/film/${movie._id}`">
+          <Movie :movie="movie" />
+        </router-link>
+      </li>
+    </ul>
+  </div>
+  <p>Les films du moment :</p>
   <div id="container-movies">
     <ul>
       <li v-for="movie in movies" :key="movie._id">
@@ -102,23 +102,36 @@ export default {
           });
       }
     },
-    // fetchMoviesR: function () {
-    //   axios
-    //     .get(`http://localhost:3000/movies`)
-    //     .then((response) => {
-    //       // Do something if call succeeded
-    //       for (let res = 0; res < 100; res++) {
-    //         this.movies.push(response.data.movies[res]);
-    //       }
-    //     })
-    //     .catch((error) => {
-    //       this.usersLoadingError = "An error occured while fetching users.";
-    //       console.error(error);
-    //     });
-    // },
+    fetchMoviesR: function () {
+      axios
+        .post(`http://localhost:3000/movies/recomandation`)
+        .then((response) => {
+          // Do something if call succeeded
+          console.log("ok");
+          for (let res = 0; res < 5; res++) {
+            var v = response.data.recom[res];
+            axios
+              .post(`http://localhost:3000/film/` + v, { id: v })
+              .then((response) => {
+                // Do something if call succeeded
+                this.movies.push(response.data.movie[0]);
+                console.log(this.movies[0].path);
+              })
+              .catch((error) => {
+                this.usersLoadingError = "An error occured while getting film.";
+                console.error(error);
+              });
+          }
+        })
+        .catch((error) => {
+          this.usersLoadingError = "An error occured while fetching users.";
+          console.error(error);
+        });
+    },
   },
   created: function () {
     this.fetchMovies();
+    this.fetchMoviesR();
   },
 };
 </script>
